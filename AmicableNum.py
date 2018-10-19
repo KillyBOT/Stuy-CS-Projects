a965d28514c9676c43e988c9257c501850b2e3a8
KillyBOT <edwards.kyle.j@gmail.com>
2018-10-18 22:30:33 -0400

    Amicable Nums
    I have finally finished the Nth amicable number finder. It's not well optimized, so you will have to wait a long time for even the most basic of amicable numbers. Keep in mind I'm not using any mathematical formula to make this, since I know one does exist. Just run the program, with the amicable number you want to find as one of the arguments. It also has functionality that may be able to find connections between three or more numbers, but as of now I think it may be impossible. Expect performance upgrades soon, and perhaps a solution to the unsolvable problem.

--- AmicableNum.py
+++ AmicableNum.py
@@ -40,11 +40,55 @@ def buildNFactorSum(n):
     return factorSumDict
 
 def findNthAmicable(n):
-    amicableNums = []
-    factorSumDict = buildNFactorSum((n*100)**2)
-    for x in range(len(factorSumDict)):
-        if factorSumDict[x] == factorSumDict[factorSumDict[x]]:
-            amicableNums.append([factorSumDict[x],factorSumDict[factorSumDict[x]]])
+    num = 2
+    numOfAmicable = 0
+    seenPair = []
+    currentAmicable = 0
 
-    return amicableNums
-print(findNthAmicable(1))
+    while numOfAmicable < n:
+        numPair = sum(findFactors(num))
+        ifNumAmicable = sum(findFactors(numPair))
+        #print(num)
+        if num == ifNumAmicable and num not in seenPair and num != numPair:
+            currentAmicable = [num,numPair]
+            numOfAmicable += 1
+            seenPair.append(numPair)
+        num += 2
+
+    return currentAmicable
+
+def buildAmicablePath(mx, n):
+    currentDegree = 1
+    nxt = sum(findFactors(n))
+    if nxt == n:
+        return False
+    path = [n,nxt]
+    #print(n,nxt)
+    while len(path) <= mx:
+        nxt = sum(findFactors(nxt))
+        if nxt == n:
+            return path
+        path.append(nxt)
+        if nxt == path[-1]:
+            return False
+    return False
+
+def findNthAmicableDegree(degree,sz):
+    place = 2
+    amicables = []
+    while place <= sz:
+        path = buildAmicablePath(degree, place)
+        if path != False:
+            amicables.append(path)
+        place += 1
+    return amicables
+    
+#print(findNthAmicableDegree(3,100000))
+#print(findNthAmicable(10))
+
+if __name__ == "__main__":
+    amicable = 3
+    if (len(sys.argv) > 1):
+        amicable = sys.argv[1]
+
+    print(findNthAmicable(amicable))
