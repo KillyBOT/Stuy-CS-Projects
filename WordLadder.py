f95f043bee1fea13dfccbd3c89e02e21da7a3b4a
KillyBOT <edwards.kyle.j@gmail.com>
2018-10-23 11:54:41 -0400

    Word Ladder 0.1

--- WordLadder.py
+++ WordLadder.py
@@ -1,1 +1,27 @@
-print("something")
+import sys,os
+import copy
+
+wordDict = open("dictall.txt","r")
+wordList = []
+    
+for word in wordDict.readlines():
+    wordList.append(word.split()[0])
+
+def findLikeWords(word):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    wordSplit = list(word)
+    likeWords = []
+    for letter in range(len(wordSplit)):
+        for singleLetter in letters:
+            current = copy.deepcopy(wordSplit)
+            current[letter] = singleLetter
+            currentWord = "".join(current)
+            if currentWord in wordList:
+                likeWords.append(currentWord)
+
+    return likeWords
+
+
+print(findLikeWords("hand"))
+#def astar(start, current, frontier, cost):
+    
