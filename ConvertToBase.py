1675f0a76213f8d6b290a88f39b6a802edd804bb
KillyBOT <edwards.kyle.j@gmail.com>
2018-10-25 19:46:25 -0400

    Convert to Base 2 - Electric Boogalo
    It's the same program and it works in the same way, except now it can take in and output numbers all the way up to base 36. Anything beyond that would require new symbols be made, so it shouldn't get any higher than that. Functionality is still the same. Do this:
    
    python3 ConvertToBase.py [Starting number] [Starting base] [Ending base]
    
    Note that the digits 11-35 are the letters a-z, like in hexadecimal.

--- ConvertToBase.py
+++ ConvertToBase.py
@@ -1,16 +1,21 @@
 import sys, os
 
+digits = "0123456789abcdefghijklmnopqrstuvwxyz"
+digitsDict = {}
+
+for digit in range(len(digits)):
+    digitsDict[digits[digit]] = digit
+
 def findUpperBound(num, base):
     currentPow = 0
     while base ** currentPow <= num:
         currentPow += 1
     return currentPow - 1
 
-#Convert from base 10 to any base, at least up to base 16
+#Convert from base 10 to any base, at least up to base 36
 def convertTo(num, base):
     if num == 0:
         return "0"
-    digits = "0123456789abcdef"
     currentNum = num
     currentPow = findUpperBound(num,base)
     returnString = []
@@ -24,34 +29,46 @@ def convertTo(num, base):
 
     return "".join(returnString)
 
-#Convert from any base to base 10, at least up to base 10
+#Convert from any base to base 10, at least up to base 36
 def convertFrom(num, base):
     final = 0
     splitNum = []
     for x in range(len(str(num))):
-        splitNum.append(int(str(num)[x]))
+        splitNum.append(str(num)[x])
 
     currentPow = len(splitNum)
 
     while currentPow > 0:
-        final += splitNum[-currentPow] * (base ** (currentPow - 1))
+        if digitsDict[splitNum[-currentPow]] > base:
+            print("You need to make sure your base matches up with your number")
+            return 0
+        final += digitsDict[splitNum[-currentPow]] * (base ** (currentPow-1))
         currentPow -= 1
 
     return final
 
+#I could probably do both conversions in one function, but it was easier for me to do them seperately.
+#Besides, that makes it easier to read!
 def convertFromTo(start, startPow, endPow):
     return convertTo(convertFrom(start,startPow),endPow)
 
 #print(findUpperBound(2,2))
 if __name__ == "__main__":
-    start = 112
-    startPow = 5
-    endPow = 2
+    start = "feea"
+    startPow = 16
+    endPow = 20
     if len(sys.argv) >= 4:
-        start = int(sys.argv[1])
+        start = str(sys.argv[1])
         startPow = int(sys.argv[2])
         endPow = int(sys.argv[3])
+    if startPow > 36:
+        print("Starting base too high! Using base 36")
+        startPow = 36
+    if endPow > 36:
+        print("Ending base too high! Using base 36")
+        endPow = 36
 
+    #print(convertFrom("ff",16))
     print(convertFromTo(start,startPow,endPow))
 
     
