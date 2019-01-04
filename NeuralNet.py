70254eb6927337aa80b9ac11f5c40c85f7214f2c
KillyBOT <edwards.kyle.j@gmail.com>
2019-01-04 11:47:57 -0500

    Neural Net V0.2
    Man, this is harder than even I thought it would be. Oh well, at least I changed some things.

--- NeuralNet.py
+++ NeuralNet.py
@@ -8,73 +8,74 @@ def sig(x):
     return 1 / (1 + exp(-x))
 
 #This class represents the links between the neurons. It's main job is backpropagation
-
-class Link(object):
-
-    def __init__(self, startNeuron, endNeuron):
-        self.startNeuron = startNeuron
-        self.endNeuron = endNeuron
-        self.weight = random.uniform(-2,2)
-
-    def getVal(self):
-        return this.startNeuron.value * this.weight
+#StartPos is k, endPos is j
 
 #This class represents the neurons in the network. It's mainly for organization.
 
 class Neuron(object):
 
-    def __init__(self, layer, layerPos, isBias):
+    def __init__(self):
         self.value = 0
-        self.enterLinks = []
-        self.endLinks = []
-        self.layer = layer
-        self.layerPos = layerPos
-        self.isBias = isBias
 
-    def setValueManual(self, value):
+    def setValue(self, value):
         self.value = value
 
-    def addEnterLink(self, link):
-        self.enterLinks.append(link)
+#This is a class that represents a layer. Again, it's mainly for organization. It contains neurons, and a bias node. Note that links are not included
+#The layer num is which layer it is, and the size is how many neurons are in the layer
 
-    def addEndLink(self, link):
-        self.endLinks.append(link)
+class Layer(object):
 
-    def setValue(self):
-        if self.layer != 0:
-            finalVal = 0
-            for link in self.enterLinks:
-                finalVal = finalVal + (link.getVal())
+    def __init__(self, layerNum, size):
+        self.layerNum = layerNum
+        self.size = size
+        self.neurons = []
+        self.bias = random.uniform(-1,1)
 
-            return sig(finalVal)
-        else:
-            return False
+        for pos in range(self.size):
+            self.neurons.append(Neuron())
+
 
 #This is the actual neural net class, and is what does the propagation
 
 class NeuralNet(object):
 
-    def __init__(self, layerData):
-        self.neurons = []
-        self.links = []
-        self.layers = len(layerData)
-        for layer in range(self.layers):
-            toAdd = []
-            for layerPos in range(layerData[layer]):
-                toAdd.append(Neuron(layer, layerPos, False))
+    def __init__(self, layerData, learningRate, epochSize, batchesPerEpoch):
+        self.layers = []
+        self.links = {}
+        self.learningRate = learningRate
+        self.epochSize = epochSize
+        self.batchesPerEpoch = batchesPerEpoch
 
-            self.neurons.append(toAdd)
+        for layer in range(len(layerData)):
+            self.layers.append(Layer(layer,layerData[layer]))
 
-        for startLayer in range(len(self.neurons)-1):
-            endLayer = startLayer + 1
-            for startLayerPos in range(len(self.neurons[startLayer])):
-                for endLayerPos in range(len(self.neurons[endLayer])):
-                    linkToAdd = Link(self.neurons[startLayer][startLayerPos],self.neurons[endLayer][endLayerPos])
-                    self.neurons[startLayer][startLayerPos].addEndLink(linkToAdd)
-                    self.neurons[endLayer][endLayerPos].addEnterLink(linkToAdd)
-                    self.links.append(linkToAdd)
+        for layer in range(len(self.layers)-1):
+            for startPos in range(self.layers[layer].size):
+                for endPos in range(self.layers[layer+1].size):
+                    #This is where the value of the links is initially set
+                    #Remember j, k, l is how I am organizing the links. J is the end position, K is the start position, and L is the start layer
+                    self.links[(endPos,startPos,layer)] = random.union(-1,1)
 
+    def setStartingNeurons(self, startingNeuronsData):
+        #You can edit this later to transalte any sort of input data into data for the starting neurons.
+        #For now, I'm doing an xor function, so the data is just a list of 1s and 0s, with 2 bytes put together (i.e. [0,1,1,1,0,1,0,1,1,0,1,0,1,1,0,0])
 
-testNet = NeuralNet([16,8,8,4])
-for link in testNet.links:
-    print(link.weight)
+        if len(startingNeuronsData) != len(self.layers[0]):
+            print("The input data is a different size than the amount of input nodes")
+            return False
+
+        for dataPos in range(len(startingNeuronsData)):
+            for pos in range(len(self.layers[0])):
+                self.layers[0].neurons[pos].setValue(0)
+                self.layers[0].neurons[pos].setValue(startingNeuronsData[dataPos])
+        return True
+
+    def propagate(self, inputData):
+        if (setStartingNeurons(inputData) == False):
+            return False
+
+        for layer in range(1,len(self.layers)):
+            for neuron in range(len(layer)):
+                finalVal = 0
+
+testNet = NeuralNet([16,8,8,4], 1, 100, 10)
